bc991a9a44d1013c1e1a4b0303702ff854acf7e1
Ellieyee <ellieyeewenna@gmail.com>
2019-09-18 12:03:27 +0800

    Tutorial adding command (#8)
    * Add Remark to model
    
    * Update storage
    
    * Fix checkstyle violations

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -38,6 +38,8 @@ public class PersonCard extends UiPart<Region> {
     private Label email;
     @FXML
     private FlowPane tags;
+    @FXML
+    private Label remark;
 
     public PersonCard(Person person, int displayedIndex) {
         super(FXML);
@@ -49,6 +51,7 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        remark.setText(person.getRemark().value);
     }
 
     @Override
